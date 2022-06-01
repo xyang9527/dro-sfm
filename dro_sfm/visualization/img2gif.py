a149a03993011b4ab64372ad9baf43891610b5d8
xyang9527 <xyang9527@gmail.com>
2022-06-01 10:46:38 +0800

    +) add text with PIL.

--- dro_sfm/visualization/img2gif.py
+++ dro_sfm/visualization/img2gif.py
@@ -3,17 +3,22 @@
 """
 References
 ----------
-  https://pythonprogramming.altervista.org/png-to-gif/
+https://pythonprogramming.altervista.org/png-to-gif/
     Png to animated Gif with Python
 
-  https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#saving
+https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#saving
     PIL GIF Saving
 
-  https://pillow.readthedocs.io/en/stable/reference/Image.html#PIL.Image.Image.save
+https://pillow.readthedocs.io/en/stable/reference/Image.html#PIL.Image.Image.save
     PIL Image save
+
+https://www.wfonts.com/font/microsoft-sans-serif
+    Free Font Microsoft Sans Serif
 """
 
 from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 import glob
 import numpy as np
 import logging
@@ -44,7 +49,23 @@ def load_images():
         #     (left, upper, right, lower)
         frames.append(new_frame.crop((60, 0, 3280, 1080)))
 
-    return frames
+    return add_text_with_pil(frames)
+
+
+def add_text_with_pil(images):
+    logging.warning(f'add_text_with_pil(..)')
+    # https://www.geeksforgeeks.org/adding-text-on-image-using-python-pil/
+    #     Adding Text on Image using Python – PIL
+    # https://stackoverflow.com/a/16377244
+
+    font = ImageFont.truetype("/home/sigma/vgithub-xyang9527/kneron_figure/ttf/micro_se.ttf", 16)
+    for idx, img in enumerate(images):
+          draw = ImageDraw.Draw(img)
+          # https://pillow.readthedocs.io/en/stable/reference/ImageDraw.html#PIL.ImageDraw.ImageDraw.text
+          draw.text((100, 300), f'Frame {idx:4d}', fill=(255, 0, 0), font=font, stroke_width=0)
+
+    return images
+
 
 def write_gif(frames):
     logging.warning(f'write_gif()')
@@ -52,17 +73,17 @@ def write_gif(frames):
     # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#saving
     im.save(out, save_all=True, append_images=[im1, im2, ...])
 
-      save_all
-      append_images
-      include_color_table
-      interlace
-      disposal
-      palette
-      optimize
-      transparency
-      duration
-      loop
-      comment
+        save_all
+        append_images
+        include_color_table
+        interlace
+        disposal
+        palette
+        optimize
+        transparency
+        duration
+        loop
+        comment
     '''
     # Save into a GIF file that loops forever
     frames[0].save('/home/sigma/vgithub-xyang9527/kneron_figure/png_to_gif.gif', format='GIF',
@@ -80,4 +101,6 @@ if __name__ == '__main__':
     write_gif(images)
 
     time_end_img2gif = time.time()
+    import PIL
+    print(f'{PIL.__version__}')
     print(f'img2gif.py elapsed {time_end_img2gif - time_beg_img2gif:.6f} seconds.')
