4233dc449c658930e74fac5e320bef7fff889b59
xyang9527 <xyang9527@gmail.com>
2022-06-20 14:49:13 +0800

    +) print relative pose.

--- dro_sfm/visualization/viz_datasets.py
+++ dro_sfm/visualization/viz_datasets.py
@@ -11,6 +11,7 @@ import numpy as np
 import subprocess
 import cv2
 from collections import OrderedDict
+import torch
 
 from dro_sfm.utils.setup_log import setup_log
 from dro_sfm.utils.image import load_image
@@ -18,6 +19,7 @@ from dro_sfm.visualization.pointcloud_downsample import generate_pointcloud_NxN
 from dro_sfm.visualization.viz_image_grid import VizImageGrid
 from dro_sfm.utils.horovod import print0
 from dro_sfm.utils.logging import pcolor
+from dro_sfm.geometry.pose_trans import matrix_to_euler_angles
 
 
 def get_datasets():
@@ -53,7 +55,7 @@ def generate_matterport_videos(names):
         sub_dirs['cam_left'] = '.jpg'
         sub_dirs['cam_left_vis'] = '.jpg'
         sub_dirs['depth_vis'] = '.jpg'
-        # sub_dirs['pose'] = '.txt'
+        sub_dirs['pose'] = '.txt'
         video_name = osp.join(item, 'summary.avi')
 
         generate_video(item, sub_dirs, 480, 640, 2, 2, video_name)
@@ -68,6 +70,13 @@ def generate_scannet_videos(names):
 
         sub_dirs = ['color', 'depth', 'pose']
     pass
+
+
+def is_image(path):
+    ext = osp.splitext(path)[1].lower()
+    if ext in ['.jpg', '.jpeg', '.png']:
+        return True
+    return False
 
 
 def generate_video(rootdir, subdirs, im_h, im_w, n_row, n_col, video_name):
@@ -91,38 +100,76 @@ def generate_video(rootdir, subdirs, im_h, im_w, n_row, n_col, video_name):
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     video_writer = cv2.VideoWriter(video_name, fourcc, fps, (canvas_col, canvas_row))
 
-    print(f'write {video_name}')
+    print(f'    -> write {video_name}')
+
+    mats = []
+    datetimes = []
+    null_image = np.full((im_h, im_w, 3), 255, np.uint8)
 
     for idx_frame, item_name in enumerate(names):
-        '''
-        filename = osp.join(rootdir, list(subdirs.keys())[0], item_name + '.jpg')
-        if osp.exists(filename):
-            data = cv2.imread(filename)
-        else:
-            continue
-        grid.subplot(0, 0, data, '(a) Left Camera')
-        '''
         has_data = False
         for idx, (k, v) in enumerate(subdirs.items()):
             filename = osp.join(rootdir, k, item_name + v)
+
             if osp.exists(filename):
                 has_data = True
-                data = cv2.imread(filename)
+                id_row = idx // n_col
+                id_col = idx % n_col
+
+                if is_image(filename):
+                    data = cv2.imread(filename)
+                    grid.subplot(id_row, id_col, data, f'({chr(ord("a") + idx)}) {k}')
+                else:  # pose
+                    data = np.genfromtxt(filename)
+                    mats.append(data)
+                    timestamp = int(np.int64(item_name) / 1e6)
+                    datetimes.append(timestamp)
+
+                    grid.subplot(id_row, id_col, null_image, f'({chr(ord("a") + idx)}) {k}')
+
+                    text = []
+                    if len(mats) > 1:
+                        rel_pose = np.matmul(np.linalg.inv(mats[-2]), mats[-1])
+                        xyz = matrix_to_euler_angles(torch.from_numpy(rel_pose[:3, :3]), 'XYZ')
+
+                        xyz_degree = xyz.detach() * 180.0 / np.math.pi
+                        dx, dy, dz = xyz_degree[:]
+
+                        text.append(f'[{idx_frame:6d}] {item_name}')
+                        text.append('to-prev-1:')
+                        text.append(f'  dt:   {datetimes[-1] - datetimes[-2]:6d} ms')
+                        text.append(f'  d_tx: {rel_pose[0, 3]*1000.0:6.3f} mm')
+                        text.append(f'  d_ty: {rel_pose[1, 3]*1000.0:6.3f} mm')
+                        text.append(f'  d_tz: {rel_pose[2, 3]*1000.0:6.3f} mm')
+                        text.append(f'  d_rx: {dx:6.3f} deg')
+                        text.append(f'  d_ry: {dy:6.3f} deg')
+                        text.append(f'  d_rz: {dz:6.3f} deg')
+
+                    if len(mats) > 5:
+                        rel_pose = np.matmul(np.linalg.inv(mats[-6]), mats[-1])
+                        xyz = matrix_to_euler_angles(torch.from_numpy(rel_pose[:3, :3]), 'XYZ')
+
+                        xyz_degree = xyz.detach() * 180.0 / np.math.pi
+                        dx, dy, dz = xyz_degree[:]
+                        text.append('to-prev-5:')
+                        text.append(f'  dt:   {datetimes[-1] - datetimes[-6]:6d} ms')
+                        text.append(f'  d_t_xyz: ({rel_pose[0, 3]*1000.0:.3f}, {rel_pose[1, 3]*1000.0:.3f}, {rel_pose[2, 3]*1000.0:.3f})')
+                        text.append(f'  d_r_xyz: ({dx:.3f}, {dy:.3f}, {dz:.3f})')
+                        pass
+                    grid.subtext(id_row, id_col, text)
             else:
                 continue
-            id_row = idx // n_col
-            id_col = idx % n_col
-            grid.subplot(id_row, id_col, data, f'({chr(ord("a") + idx)}) {k}')
+        # // for idx, (k, v) in enumerate(subdirs.items()):
+
         if not has_data:
             continue
 
         video_writer.write(grid.canvas)
-        if idx_frame > 1000:
+        if idx_frame > 300:
             break
-        # print(f'write {filename}')
-    video_writer.release()
 
-    pass
+    # // for idx_frame, item_name in enumerate(names):
+    video_writer.release()
 
 
 if __name__ == '__main__':
--- dro_sfm/visualization/viz_image_grid.py
+++ dro_sfm/visualization/viz_image_grid.py
@@ -6,6 +6,42 @@ import numpy as np
 import cv2
 
 
+class Colors:
+    def __init__(self):
+        # https://learn.leighcotnoir.com/artspeak/elements-color/primary-colors/
+        self.rgb = {
+            'red': (0, 0, 255),
+            'green': (0, 255, 0),
+            'blue': (255, 0, 0),
+            'yellow': (0, 255, 255),
+            'cyan': (255, 255, 0),
+            'magenta': (255, 0, 255)
+        }
+
+    @property
+    def red(self):
+        return self.rgb['red']
+
+    @property
+    def green(self):
+        return self.rgb['green']
+
+    @property
+    def blue(self):
+        return self.rgb['blue']
+
+    @property
+    def yellow(self):
+        return self.rgb['yellow']
+
+    @property
+    def cyan(self):
+        return self.rgb['cyan']
+
+    @property
+    def magenta(self):
+        return self.rgb['magenta']
+
 class VizImageGrid:
     """
        (0, 0) ------------------------------------> col
@@ -21,8 +57,7 @@ class VizImageGrid:
     """
 
     def __init__(self, cell_row, cell_col, grid_row, grid_col):
-        logging.warning('__init__( {}, {}, {}, {} )'.format(
-            cell_row, cell_col, grid_row, grid_col))
+        logging.warning(f'__init__({cell_row}, {cell_col}, {grid_row}, {grid_col})')
 
         self.cell_row = cell_row  # 480
         self.cell_col = cell_col  # 640
@@ -42,6 +77,7 @@ class VizImageGrid:
                            + self.border_sz * 2)
 
         self.color_bg = 128
+        self.colors = Colors()
 
         self.canvas = np.full((self.canvas_row, self.canvas_col, 3),
                               self.color_bg, np.uint8)
@@ -65,16 +101,16 @@ class VizImageGrid:
         self.font_face_caption = cv2.FONT_HERSHEY_DUPLEX
         self.font_scale_caption = 1.2
         self.thickness_caption = 3
-        self.color_caption = (0, 0, 255)
+        self.color_caption = self.colors.red  # (0, 0, 255)
+
         self.font_face_subcaption = cv2.FONT_HERSHEY_COMPLEX
         self.font_scale_subcaption = 0.8
         self.thickness_subcaption = 2
-        self.color_subcaption = (0, 255, 255)
-        self.color_subsubcaption = (255, 0, 255)
+        self.color_subcaption = self.colors.yellow  # (0, 255, 255)
+        self.color_subsubcaption = self.colors.magenta  # (255, 0, 255)
 
-    def _check_input(self, row, col, data):
-        if not isinstance(row, int) or not isinstance(col, int)\
-                or not isinstance(data, np.ndarray):
+    def _check_input(self, row, col, data=None):
+        if not isinstance(row, int) or not isinstance(col, int):
             raise ValueError
 
         if row < 0 or row >= self.grid_row:
@@ -82,16 +118,21 @@ class VizImageGrid:
         if col < 0 or col >= self.grid_col:
             raise ValueError
 
-        data_shape = data.shape
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise ValueError
 
-        if len(data_shape) != 3:
-            raise ValueError
-        if data_shape[0] != self.cell_row or data_shape[1] != self.cell_col \
-                or data_shape[2] != 3:
-            raise ValueError
+            data_shape = data.shape
+
+            if len(data_shape) != 3:
+                raise ValueError
+
+            if data_shape[0] != self.cell_row or data_shape[1] != self.cell_col \
+                    or data_shape[2] != 3:
+                raise ValueError
 
     def caption(self, caption_text):
-        logging.warning('caption( {} )'.format(caption_text))
+        logging.warning(f'caption({caption_text})')
 
         # compute offset
         '''
@@ -131,13 +172,16 @@ class VizImageGrid:
         data : numpy.ndarray
         caption_text : str
         """
-        logging.warning('subplot( {}, {}, {} )'.format(row, col, data.shape))
+        logging.warning(f'subplot({row}, {col}, {data.shape})')
 
         self._check_input(row, col, data)
+
         row_beg = (self.cell_row + self.gap_sz) * row + self.border_sz
         row_end = row_beg + self.cell_row
+
         col_beg = (self.cell_col + self.gap_sz) * col + self.border_sz
         col_end = col_beg + self.cell_col
+
         self.canvas[row_beg:row_end, col_beg:col_end, :] = data
 
         if caption_text != '':
@@ -157,16 +201,56 @@ class VizImageGrid:
                     color = self.color_subsubcaption
 
                 pos_col = col_beg + self.text_offset
-                pos_row = row_beg + self.text_offset + i * box_row * 2
-                pos_row = row_end + 30
+                # pos_row = row_beg + self.text_offset + i * box_row * 2
+                pos_row = row_end + self.text_offset
 
                 cv2.putText(self.canvas, str_line, (pos_col, pos_row),
                             self.font_face_subcaption,
                             self.font_scale_subcaption,
                             color, self.thickness_subcaption)
 
+    def subtext(self, row, col, text, caption=None):
+        logging.warning(f'subtext({row}, {col}, {text})')
+
+        self._check_input(row, col)
+        row_beg = (self.cell_row + self.gap_sz) * row + self.border_sz
+        row_end = row_beg + self.cell_row
+        col_beg = (self.cell_col + self.gap_sz) * col + self.border_sz
+
+        # text
+        for idx_line, str_line in enumerate(text):
+            retval, _ = cv2.getTextSize(
+                str_line, self.font_face_subcaption, self.font_scale_subcaption,
+                self.thickness_subcaption)
+            _, box_row = retval
+
+            pos_col = col_beg + self.text_offset
+            pos_row = row_beg + self.text_offset + box_row * idx_line * 2
+
+            '''
+            cv2.putText(self.canvas, str_line, org=(pos_col, pos_row),
+                        fontFace=self.font_face_subcaption,
+                        fontScale=self.font_scale_subcaption,
+                        color=self.colors.magenta, thickness=self.thickness_subcaption)
+            '''
+            cv2.putText(self.canvas, str_line, org=(pos_col, pos_row),
+                        fontFace=cv2.LINE_AA,
+                        fontScale=1,
+                        color=self.colors.magenta, thickness=2)
+
+        # caption
+        if caption is not None:
+            pos_col = col_beg + self.text_offset
+            pos_row = row_end + self.text_offset
+
+            cv2.putText(self.canvas, caption, (pos_col, pos_row),
+                        self.font_face_subcaption,
+                        self.font_scale_subcaption,
+                        self.colors.yellow, self.thickness_subcaption)
+
+
     def save_fig(self, path):
-        logging.warning('save_fig( {} )'.format(path))
+        logging.warning(f'save_fig({path})')
         cv2.imwrite(path, self.canvas)
 
     def create_palette(self, title, names, colors, use_rect):
@@ -179,8 +263,8 @@ class VizImageGrid:
         colors : numpy.ndarray
         use_rect : bool
         """
-        logging.warning('create_palette( {}, {}, {} )'.format(
-            title, names, colors.shape))
+        logging.warning(f'create_palette({title}, {names}, {colors.shape})')
+
         if colors.shape[0] != len(list(names)):
             raise ValueError
 
@@ -256,7 +340,7 @@ class VizImageGrid:
         return img_palette
 
     def add_lane_matching(self, img, matching, color_key, color_value):
-        logging.warning('add_lane_matching( .. )')
+        logging.warning(f'add_lane_matching( .. )')
 
         font_title = cv2.FONT_HERSHEY_SIMPLEX
 
